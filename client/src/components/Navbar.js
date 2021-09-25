7942dfa6a2ab958fc4ffaf700e4312a7c3a6eb60
Kerry <kpierrelys.kp@gmail.com>
2021-09-25 17:26:17 -0400

    removed image folder

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = ({ toggle }) => {
     return (
         <div className='navbar'>
             <div className='nav-content'>
-                <h1>Sh<img src='images/icons8-camera-100.png' alt='Camera logo'/>tbyKerry</h1>
+                <h1>Sh<img src='photos/icons8-camera-100.png' alt='Camera logo'/>tbyKerry</h1>
                 <i onClick={toggle} className="fas fa-bars fa-2x"></i>
             </div>
         </div>
--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -6,7 +6,7 @@ const SideBar = ({ close, focusOnPortfolioWithoutFeature, focusOnAboutMeWithOutF
     return (
         <div>
             <div className={isActive ? 'show-sidebar' : 'sidebar'}>
-                <h1>Sh<img src='images/icons8-camera-100.png' alt='Camera logo'/>tbyKerry</h1>
+                <h1>Sh<img src='photos/icons8-camera-100.png' alt='Camera logo'/>tbyKerry</h1>
                 <p onClick={close} id={isActive ? 'show-close' : 'hide-close'}>Close</p>
                 {isActive ? <p onClick={focusOnPortfolio}>Portfolio</p> : <p onClick={focusOnPortfolioWithoutFeature}>Portfolio</p>}
                 {isActive ? <p onClick={focusOnAboutMe}>About Me</p> : <p onClick={focusOnAboutMeWithOutFeature}>About Me</p>}
