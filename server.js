e5841700ec05dc57cb8333ac1d52369c6bf33608
Kerry <kpierrelys.kp@gmail.com>
2021-09-25 19:23:53 -0400

    added credentials

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const nodemailer = require("nodemailer");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { google } = require('googleapis');
 
 app.use(express.json());
 app.use(cors());
+
+const oAuth2Client = new google.auth.OAuth2(process.env.OAUTH_CLIENTID, process.env.OAUTH_CLIENT_SECRET, process.env.REDIRECT_URI);
+
+oAuth2Client.setCredentials({ refresh_token: process.env.OAUTH_REFRESH_TOKEN });
+
+const accessToken = oAuth2Client.getAccessToken();
 
 let transporter = nodemailer.createTransport({
  service: "gmail",
@@ -16,6 +23,7 @@ let transporter = nodemailer.createTransport({
    clientId: process.env.OAUTH_CLIENTID,
    clientSecret: process.env.OAUTH_CLIENT_SECRET,
    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+   accessToken: accessToken,
  },
 });
 transporter.verify((err, success) => {
